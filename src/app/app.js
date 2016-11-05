07838788fbb79d7b6feefbf360007c6710f1a679
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-11-05 00:01:37 -1000

    merge ui-router branch from marta

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
-
+import * as uiRouter from 'angular-ui-router';
+import { InputBoxCtrlState, InputBoxCtrl, InputBoxCtrlName } from './inputbox';
 import '../style/app.css';
 
 let app = () => {
@@ -18,8 +19,13 @@ class AppCtrl {
 
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [])
+angular.module(MODULE_NAME, ['ui.router'])
+.config(($stateProvider) => {
+    $stateProvider
+      .state('inputbox', InputBoxCtrlState);
+  })
   .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+  .controller('AppCtrl', AppCtrl)
+  .controller(InputBoxCtrlName, InputBoxCtrl);
 
 export default MODULE_NAME;
