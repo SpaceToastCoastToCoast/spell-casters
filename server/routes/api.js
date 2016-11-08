d2fe5a78a5dfc6ca9b615dc4b4417341ad4fee0e
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-11-08 12:02:26 -1000

    main character animation loads and displays properly in the game view

--- server/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const words = require('../data/test.json');
-
-router.get('/words', (req,res) => {
-  res.json(words);
-});
-
-module.exports= router;
