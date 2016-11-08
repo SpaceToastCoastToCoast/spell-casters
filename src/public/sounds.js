bb7768299d8b707ac65494fca4edb04ff1a95cb2
Renee <rchar20@gmail.com>
2016-11-08 13:17:01 -1000

    combines time limit onto word_dataset
    - will redirect to game-over page if they run out of time on a level
    - will reset the timer each time they reach a new level
    - will stop the timer when they reach the end of the game
    - will keep track of how many seconds they had left when they completed a level
    
    resolves #51

--- src/public/sounds.js
+++ src/public/sounds.js
@@ -2,7 +2,7 @@
 
 var sound = new Howl({
   src: ['music/Spellcasters_short_theme.ogg'],
-  autoplay: true,
-  loop: true
+  // autoplay: true,
+  // loop: true
 });
-sound.play();
+// sound.play();
