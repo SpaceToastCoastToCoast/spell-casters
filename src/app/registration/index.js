5af4a1ab117ba57b95722784407be2e85a3ffee5
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-11-10 15:08:54 -1000

    adding stats to won page

--- src/app/registration/index.js
+++ src/app/registration/index.js
@@ -25,27 +25,24 @@ export const RegistrationServices = [
 
   registerUser (registerData) {
     this.data = registerData;
-    const req ={
+    const req = {
       method: 'POST',
       url: `/api/register`,
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       },
       data: `username=${registerData.username}&password=${registerData.password}&=`
-     };
-     console.log('req: ', req);
-     return this.$http(req).success(response => {
-        if(response.success === true){
-          this.$state.go('splash', {registrationMessage: response.registrationMessage});
-        }else{
-          this.$state.go('registration', {registrationMessage: response.registrationMessage});
-        }
-       return response;
-     });
-
+    };
+    return this.$http(req).success(response => {
+      if(response.success === true){
+        this.$state.go('splash', {registrationMessage: response.registrationMessage});
+      } else {
+        this.$state.go('registration', {registrationMessage: response.registrationMessage});
+      }
+     return response;
+    });
   }
- }
-];
+}];
 
 export const RegistrationCtrl = [
   '$scope', '$state', '$stateParams', 'RegistrationServices',
