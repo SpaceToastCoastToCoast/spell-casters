29e6761e52d640dc92bfcd0e50f84e2e69381b0e
MartaKatona <marta.katona.us@gmail.com>
2016-11-25 20:19:05 -1000

    wip intalls karma, jasmin and karma-webpack and configures karma

--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -16,5 +16,42 @@ describe('app', () => {
     it('should contain the starter url', () => {
       expect(ctrl.url).toBe('https://github.com/preboot/angular-webpack');
     });
+
+    //
+    it('should have a LoginCtrl controller', function() {
+      expect(App.LoginCtrl).toBeDefined();
+    });
+
+    describe('LoginCtrl', () => {
+      let ctrl;
+      let scope = {};
+
+      beforeEach(() => {
+        angular.mock.module(app);
+
+        angular.mock.inject(($controller) => {
+          ctrl = $controller('LoginCtrl', {$scope:scope});
+        });
+      });
+
+      it('shold initialize the userName and password in the scope', ()=> {
+        expect(scope.userName).toBeDefined();
+        expect(scope.password).toBeDefined();
+      });
+
+    });
+
   });
+
+
+
+
+
+
+
+  // it('should have a working UserServices service', inject(['UserServices',
+  //   function(UserServices) {
+  //     expect(UserServices.getUsers).not.to.equal(null);
+  // }]));
+
 });
---
+++ src/app/login/user_service.spec.js
@@ -0,0 +1,23 @@
+describe('user_service', function() {
+
+  it('has a dummy spec to test 2+2', function() {
+    expect(2+2).toEqual(4);
+  });
+
+  it('ensures user can log in', function() {
+    // expect current scope to contain username
+  });
+  it('ensures path has changed', function() {
+    // expect path to equal '/dashboard'
+  });
+  //critical
+  it('ensures invalid username is caught', function() {});
+  it('ensures valid username pass validation', function() {});
+  it('ensures submitting form changes path', function() { });
+  //nice
+  it('ensures client-side helper message shown for empty fields', function() {})
+  it('ensures hitting enter on password field submits for', function() {})
+
+
+
+});
